0262f9c22a7befaadde7ba74c8caebc5e31ee7b9
Himanshi Sharma <himanshish456@gmail.com>
2024-03-07 20:51:12 +0530

    navbar responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Cards from "./components/Cards";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <div className="min-h-screen">
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const lists = [
   {
@@ -109,51 +110,84 @@ const Navbar = () => {
   };
 
   return (
-    <div>
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-row">
+    <header className="w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300">
+      <div className="md:flex flex-col items-center justify-center lg:flex-row lg:justify-between">
+        <div className="flex flex-row items-center lg:items-start lg:ml-40 justify-center">
           <img className="w-20" src={logo} alt="" />
-          <h3>Policy Pulse</h3>
+          <h3 className="mt-4 text-green-500 text-4xl font-bold italic font-serif">
+            POLICY PULSE
+          </h3>
         </div>
-        <div className="flex flex-row gap-20">
+        <div className="flex flex-col gap-5 items-center lg:flex-row lg:gap-20 lg:mr-40">
           <div className="flex flex-col">
-            <h3>Call us on</h3>
-            <h3>9818321882</h3>
-            <h3>9818122882</h3>
+            <h6 className="ml-auto">CALL US ON</h6>
+            <h2 className="text-gray-500">+91 - 9818321882</h2>
+            <h2 className="text-gray-500">+91 - 9818122882</h2>
           </div>
-          <div className="flex flex-col">
-            <h3>Email us on</h3>
-            <h3>liclokeshsharma@gmail.com</h3>
+          <div className="flex flex-col ">
+            <h6 className="ml-auto">EMAIL US ON</h6>
+            <h4 className="text-gray-500 text-xs">liclokeshsharma@gmail.com</h4>
           </div>
         </div>
       </div>
-      <div onClick={handleClick}>
-        <div>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-        </div>
-        <ul className="flex flex-row gap-20">
+      <div
+        className={`bg-gray-400 w-full p-5 absolute cursor-pointer items-center justify-center ${
+          click ? "h-auto" : "h-15"
+        }`}
+      >
+        {click ? (
+          <AiOutlineClose
+            onClick={handleClick}
+            className="lg:hidden md:hidden"
+          />
+        ) : (
+          <div className="flex flex-row justify-between lg:hidden md:hidden">
+            <h1 className="text-white font-bold italic text-2xl ">MENU</h1>
+            <AiOutlineMenu
+              onClick={handleClick}
+              className="lg:hidden md:hidden mt-2"
+            />
+          </div>
+        )}
+        <ul
+          className={`md:flex flex-col md:flex-row lg:ml-40 gap-10  ${
+            click ? "flex" : "hidden"
+          }`}
+        >
           {lists.map((list, index) => (
             <li
               key={index}
               onMouseEnter={() => handleDropdown(index)}
               onMouseLeave={() => handleDropdown(null)}
+              className={`lg:bg-gray-400 ${
+                window.innerWidth < 640
+                  ? "sm:bg-gray-200 flex justify-center h-10 rounded-3xl pt-1"
+                  : ""
+              }`}
             >
               <Link to="/">
                 {list.header} <i className="fas fa-caret-down" />
               </Link>
               {hoveredIndex === index && (
-                <ul className="dropdown-menu">
+                <ul className="absolute bg-gray-200 flex flex-col gap-1 top-15 border-r-8">
                   {list.sublists &&
                     list.sublists.map((sublist, subIndex) => (
                       <div key={subIndex}>
                         {list.links && list.links[subIndex] ? (
                           <Link to={list.links[subIndex]}>
-                            <li>{sublist}</li>
+                            <li className="items-center text-sm ml-1">
+                              {sublist}
+                            </li>
                           </Link>
                         ) : (
                           <li>{sublist}</li>
                         )}
-                        {subIndex !== list.sublists.length - 1 && <hr />}
+                        {subIndex !== list.sublists.length - 1 && (
+                          <div class="flex items-center">
+                            <div class="w-24 h-1 border-b border-dotted border-black"></div>
+                            <div class="w-24 h-1 border-b border-dotted border-black"></div>
+                          </div>
+                        )}
                       </div>
                     ))}
                 </ul>
@@ -162,7 +196,7 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
-    </div>
+    </header>
   );
 };
 
