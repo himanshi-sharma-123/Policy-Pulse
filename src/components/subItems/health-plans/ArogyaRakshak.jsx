5a20b43c28ec31157d5e2dc0e429ddec61314bc2
Himanshi Sharma <himanshish456@gmail.com>
2024-03-10 11:42:11 +0530

    add subitems content

--- src/components/subItems/health-plans/ArogyaRakshak.jsx
+++ src/components/subItems/health-plans/ArogyaRakshak.jsx
@@ -1,7 +1,92 @@
 import React from "react";
+import Contact from "../../../home/Contact";
 
 const ArogyaRakshak = () => {
-  return <div> Rakshak</div>;
+  return (
+    <div className="flex flex-col md:flex-row lg:mt-20 ml-20 sm:mt-60">
+      <div className="md:w-1/2 md:ml-20">
+        <h1 className="text-5xl text-gray-500 lg:mt-20 sm:mt-20">
+          AROGYA RAKSHAK
+        </h1>
+        <div className="mt-10 item-center">
+          <p className="">
+            This is a Non-Linked, Non-Participating, Regular Premium,
+            Individual, Health Insurance plan which provides fixed benefits in
+            case of hospitalization and for various surgical procedures
+            irrespective of actual cost incurred and the benefit is in addition
+            to any other health insurance cover that insured lives may have.
+          </p>
+          <h1 className=" mt-4 font-mono font-bold text-gray-500">
+            The Benefits offered under the plan are:
+          </h1>
+          <h1 className="font-mono font-bold text-gray-500">
+            Major Surgical Benefit (MSB)
+          </h1>
+          <p className="text-left font-mono">
+            Two riders: New Term Assurance and Accident Benefit Rider available
+            for PI and Insured Spouse only. The Principle Insured (PI) can take
+            the policy covering himself/herself. The Spouse, Children, Parents
+            and Parents-in-law can also be covered under the same policy.
+          </p>
+          <h1 className="text-left font-mono">
+            For Principal Insured/Spouse: 18 Years
+          </h1>
+          <h1 className="text-left font-mono">
+            For Parents/Parents-In-Law: 18 Years
+          </h1>
+        </div>
+      </div>
+      <div className="md:w-2/6 md:mr-20 lg:mt-10 lg:ml-20 md:ml-20 sm:mr-20">
+        <Contact />
+      </div>
+    </div>
+
+    // <div className="px-4 lg:px-24 flex items-center bg-cyan-200">
+    //   <div className="flex w-full flex-col md:flex-row justify-between items-center py-10 md:py-20 lg:py-40">
+    //     <div className="flex flex-col md:flex-col lg:flex-row">
+    //       <div className="md:ml-20 md:mt-14 sm:mt-60">
+    //         <h1 className="text-3xl font-bold">AROGYA RAKSHAK</h1>
+    //         <p>Product Summary</p>
+    //         <p>
+    //           This is a Non-Linked, Non-Participating, Regular Premium,
+    //           Individual, Health Insurance plan which provides fixed benefits in
+    //           case of hospitalization and for various surgical procedures
+    //           irrespective of actual cost incurred and the benefit is in
+    //           addition to any other health insurance cover that insured lives
+    //           may have.
+    //         </p>
+    //         <p>The Benefits offered under the plan are:</p>
+    //         <ol>
+    //           <li>Hospital Cash Benefit (HCB)</li>
+    //           <li>Major Surgical Benefit (MSB)</li>
+    //           <li>
+    //             Day Care Procedure Benefit (DCPB) Other Surgical Benefit (OSB)
+    //           </li>
+    //         </ol>
+    //         <p>These are in addition to any other health insurance cover.</p>
+    //         <p>
+    //           Two riders: New Term Assurance and Accident Benefit Rider
+    //           available for PI and Insured Spouse only. The Principle Insured
+    //           (PI) can take the policy covering himself/herself. The Spouse,
+    //           Children, Parents and Parents-in-law can also be covered under the
+    //           same policy.
+    //         </p>
+    //         <h1>Premium Payment Mode:</h1>
+    //         <p>Yearly, Halfly</p>
+    //         <h1>Minimum Entry Age :</h1>
+    //         <ul>
+    //           <li>For Principal Insured/Spouse: 18 Years</li>
+    //           <li>For Child: 3 Months</li>
+    //           <li>For Parents/Parents-In-Law: 18 Years</li>
+    //         </ul>
+    //       </div>
+    //       <div className="md:mr-10">
+    //         <Contact />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+  );
 };
 
 export default ArogyaRakshak;
